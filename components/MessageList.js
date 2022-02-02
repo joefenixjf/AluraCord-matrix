1c13859f7d7a9cdb2d57b6647e82b28a4fcfbf0a
Joe Fenix <joefenix.jf@gmail.com>
2022-02-01 21:50:32 -0300

    Desafio Botoes
    desafio de botoes, Enviar e Deletar

---
+++ components/MessageList.js
@@ -0,0 +1,82 @@
+import { Box, Text, Image, Button } from "@skynexui/components";
+import appConfig from "../config.json";
+
+export default function MessageList({ mensagens, deletaMsg }) {
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "hidden",
+        overflowY: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px",
+      }}
+    >
+      {mensagens.map((mensagem) => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700],
+              },
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: "8px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <Image
+                  styleSheet={{
+                    width: "20px",
+                    height: "20px",
+                    borderRadius: "50%",
+                    display: "inline-block",
+                    marginRight: "8px",
+                  }}
+                  src={`https://github.com/vanessametonini.png`}
+                />
+                <Text tag="strong">{mensagem.de}</Text>
+                <Text
+                  styleSheet={{
+                    fontSize: "10px",
+                    marginLeft: "8px",
+                    color: appConfig.theme.colors.neutrals[300],
+                  }}
+                  tag="span"
+                >
+                  {new Date().toLocaleDateString()}
+                </Text>
+              </div>
+              <Button
+                onClick={() => {
+                  deletaMsg(mensagem.id);
+                }}
+                label="X"
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.warnings[100],
+                  mainColorLight: appConfig.theme.colors.warnings[500],
+                  mainColorStrong: appConfig.theme.colors.warnings[800],
+                }}
+              />
+            </Box>
+            {mensagem.texto}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
--- pages/chat.js
+++ pages/chat.js
@@ -1,11 +1,21 @@
 import React from "react";
 import appConfig from "../config.json";
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import Header from "../components/Header";
+import MessageList from "../components/MessageList";
 
 export default function ChatPage() {
   // Sua lógica vai aqui
   const [mensagem, setMensagem] = React.useState("");
   const [listaMensagem, setListaMensagem] = React.useState([]);
+
+  const filtraLista = (id) => {
+    setListaMensagem([
+      ...listaMensagem.filter((i) => {
+        return i.id !== id;
+      }),
+    ]);
+  };
 
   const handleNovaMensagem = (novaMensagem) => {
     const mensagem = {
@@ -60,7 +70,7 @@ export default function ChatPage() {
           }}
         >
           {/* {listaMensagem.map((mensagem) => {  })} */}
-          <MessageList mensagens={listaMensagem} />
+          <MessageList mensagens={listaMensagem} deletaMsg={filtraLista} />
           <Box
             as="form"
             styleSheet={{
@@ -110,93 +120,6 @@ export default function ChatPage() {
           </Box>
         </Box>
       </Box>
-    </Box>
-  );
-}
-
-function Header() {
-  return (
-    <>
-      <Box
-        styleSheet={{
-          width: "100%",
-          marginBottom: "16px",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text variant="heading5">Chat</Text>
-        <Button
-          variant="tertiary"
-          colorVariant="neutral"
-          label="Logout"
-          href="/"
-        />
-      </Box>
-    </>
-  );
-}
-
-function MessageList(props) {
-  return (
-    <Box
-      tag="ul"
-      styleSheet={{
-        overflow: "hidden",
-        overflowY: "scroll",
-        display: "flex",
-        flexDirection: "column-reverse",
-        flex: 1,
-        color: appConfig.theme.colors.neutrals["000"],
-        marginBottom: "16px",
-      }}
-    >
-      {props.mensagens.map((mensagem) => {
-        return (
-          <Text
-            key={mensagem.id}
-            tag="li"
-            styleSheet={{
-              borderRadius: "5px",
-              padding: "6px",
-              marginBottom: "12px",
-              hover: {
-                backgroundColor: appConfig.theme.colors.neutrals[700],
-              },
-            }}
-          >
-            <Box
-              styleSheet={{
-                marginBottom: "8px",
-              }}
-            >
-              <Image
-                styleSheet={{
-                  width: "20px",
-                  height: "20px",
-                  borderRadius: "50%",
-                  display: "inline-block",
-                  marginRight: "8px",
-                }}
-                src={`https://github.com/vanessametonini.png`}
-              />
-              <Text tag="strong">{mensagem.de}</Text>
-              <Text
-                styleSheet={{
-                  fontSize: "10px",
-                  marginLeft: "8px",
-                  color: appConfig.theme.colors.neutrals[300],
-                }}
-                tag="span"
-              >
-                {new Date().toLocaleDateString()}
-              </Text>
-            </Box>
-            {mensagem.texto}
-          </Text>
-        );
-      })}
     </Box>
   );
 }
